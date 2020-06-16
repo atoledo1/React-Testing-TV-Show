1aa7e7f9d96e40d662533cd64a77b1824adbda3e
adriana <atoledosantiago1@gmail.com>
2020-06-16 02:42:38 -0500

    mvp

---
+++ src/api/fetchShow.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const fetchShow = async () => {
+  try {
+    const res = await axios.get(
+      "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+    );
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+};
